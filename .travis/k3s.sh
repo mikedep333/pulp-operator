44c4d74a6e5d1da8aa18916f65c3f08206da4a04
Mike DePaulo <mikedep333@redhat.com>
2019-07-16 10:19:10 -0400

    k3s: Wait for both external services

--- .travis/k3s.sh
+++ .travis/k3s.sh
@@ -8,7 +8,8 @@ sudo ./k3s-up.sh
 for tries in {1..30}
 do
   sleep 5
-  if sudo k3s kubectl get services | grep NodePort ; then
+  if [[ $(sudo k3s kubectl get services | grep -c NodePort) -eq 2 ]] ; then
+    sudo k3s kubectl get services
     exit 0
   fi
 done
