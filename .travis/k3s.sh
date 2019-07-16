82dfe154a65bb2cd9e34deee014e198e5690e2ae
Mike DePaulo <mikedep333@redhat.com>
2019-07-16 12:28:23 -0400

    Revert "k3s.sh: Use alias for kubectl"
    This reverts commit 5fbdba2978b3d2ef6b6d938a0ceed2f399869477.
    
    It seems to not use sudo at one point or another. And probably kubectl is a symlink.

--- .travis/k3s.sh
+++ .travis/k3s.sh
@@ -4,13 +4,12 @@
 curl -sfL https://get.k3s.io | sudo sh -
 sudo k3s kubectl get node
 sudo ./k3s-up.sh
-alias kubectl="sudo k3s kubectl"
 
 # Once the services are both up, the pods will be in a Pending state.
 # Before the services are both up, the pods may not exist at all.
 # So check for the services being up 1st.
 for tries in {0..30}; do
-  services=$(kubectl get services)
+  services=$(sudo k3s kubectl get services)
   if [[ $(echo "$services" | grep -c NodePort) -eq 2 ]]; then
     # parse string like this. 30805 is the external port
     # pulp-api     NodePort    10.43.170.79   <none>        24817:30805/TCP   0s
@@ -31,10 +30,10 @@ for tries in {0..30}; do
 done   
 
 echo "VOLUMES:"
-kubectl get pvc
+sudo k3s kubectl get pvc
 
 for tries in {0..60}; do
-  pods=$(kubectl get pods)
+  pods=$(sudo k3s kubectl get pods)
   if [[ $(echo "$pods" | grep -c Pending) -eq 0 ]]; then
     echo "PODS:"
     echo "$pods"
@@ -45,7 +44,7 @@ for tries in {0..60}; do
       echo "PODS:"
       echo "$pods"
       echo "VOLUMES:"
-      kubectl get pvc
+      sudo k3s kubectl get pvc
       exit 3
     fi
   fi
@@ -53,7 +52,7 @@ for tries in {0..60}; do
 done
 
 echo "VOLUMES:"
-kubectl get pvc
+sudo k3s kubectl get pvc
 
 URL=http://$API_IP:$API_PORT/pulp/api/v3/status/
 echo "URL:"
