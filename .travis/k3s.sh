4eb1aaec0e4d0eea3bf53dca143778492fe17e93
Mike DePaulo <mikedep333@redhat.com>
2019-07-16 12:37:09 -0400

    Fix PVCs by using hostPath as the StorageClass

--- .travis/k3s.sh
+++ .travis/k3s.sh
@@ -3,6 +3,11 @@
 
 curl -sfL https://get.k3s.io | sudo sh -
 sudo k3s kubectl get node
+# By default, k3s lacks a storage class.
+# https://github.com/rancher/k3s/issues/85#issuecomment-468293334
+# This is the way to add a simple hostPath storage class.
+sudo k3s kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
 sudo ./k3s-up.sh
 
 # Once the services are both up, the pods will be in a Pending state.
