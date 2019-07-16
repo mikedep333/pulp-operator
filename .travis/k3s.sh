2c021e168b559fe0d314b92ad56b0f8864103096
Mike DePaulo <mikedep333@redhat.com>
2019-07-16 09:55:14 -0400

    Have Travis use k3s to test the containers.
    Temporarily disable Ansible molecule, which is not working.

---
+++ .travis/k3s.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# coding=utf-8
+
+curl -sfL https://get.k3s.io | sudo sh -
+sudo k3s kubectl get node
+sudo ./k3s-up.sh
+
+for tries in {1..30}
+do
+  sleep 5
+  if k3s kubectl services | grep NodePort ; then
+    exit 0
+  fi
+done
+exit 1
---
+++ k3s-up.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias kubectl="k3s kubectl"
+. ./up.sh
