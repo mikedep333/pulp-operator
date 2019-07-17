f8c190d61ba91bbcf431881dc477094558e012ca
Mike DePaulo <mikedep333@redhat.com>
2019-07-17 17:25:37 -0400

    Fix specifying service-node-port-range

--- .travis/k3s-install.sh
+++ .travis/k3s-install.sh
@@ -5,7 +5,7 @@
 # Does a bunch of stuff, such as setting up a `kubectl` -> `k3s kubectl` symlink.
 # We want to allow devs to use Pulp's ports, like 80, 24816 or 24817,
 # not just the default 30000-32767.
-curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="--kube-apiserver-arg --service-node-port-range=80-32767" sh -
+curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=80-32767" sh -
 echo "SYSTEMD UNIT:"
 sudo cat /etc/systemd/system/k3s.service
 sudo kubectl get node
