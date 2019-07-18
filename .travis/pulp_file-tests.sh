e57c3bfd4c910c0ebe787dcb2c372888cdcbddea
Mike DePaulo <mikedep333@redhat.com>
2019-07-18 19:03:00 -0400

    Workaround Ubuntu issue with the pulp_file tests.

--- .travis/pulp_file-tests.sh
+++ .travis/pulp_file-tests.sh
@@ -5,8 +5,10 @@ pushd pulp_file/docs/_scripts
 # Let's only do sync tests.
 # So as to check that Pulp can work in containers, including writing to disk.
 # If the upload tests are simpler in the long run, just use them.
-set -x
-source docs_check_sync_publish.sh
-set +x
+#
+# TODO: Investigate why launching this script the regular way
+# works in Fedora, but not in Ubuntu.
+# Probably because sh is bash in Fedora, but dash in Ubuntu.
+bash -x docs_check_sync_publish.sh
 popd
 
