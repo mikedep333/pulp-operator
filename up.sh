780155669dc4cc34528af5dec02ccb60bb227438
Mike DePaulo <mikedep333@redhat.com>
2019-07-18 18:05:59 -0400

    Add different ConfigMaps

--- up.sh
+++ up.sh
@@ -6,6 +6,15 @@
 kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
 kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
 
+if [[ -e deploy/pulp-operator.config-map.yml ]]; then
+  kubectl apply -f deploy/pulp-operator.config-map.yml
+elif [[ "$TRAVIS" == "true" ]]; then
+  kubectl apply -f deploy/pulp-operator.travis.config-map.yml
+elif [[ "$(hostname)" == "pulp-demo"* ]]; then
+  kubectl apply -f deploy/pulp-operator.pulp-demo.config-map.yml
+else
+  kubectl apply -f deploy/pulp-operator.default.config-map.yml
+fi
 kubectl apply -f deploy/pulp-operator.config-map.yml
 kubectl apply -f deploy/service_account.yaml
 kubectl apply -f deploy/role.yaml
