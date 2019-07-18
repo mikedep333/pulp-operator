a545f27728fbebb22916d1acc788f73674370635
Mike DePaulo <mikedep333@redhat.com>
2019-07-18 17:23:07 -0400

    Use pulp-file-storage settings from a ConfigMap

--- up.sh
+++ up.sh
@@ -6,6 +6,7 @@
 kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_crd.yaml
 kubectl apply -f deploy/crds/pulpproject_v1alpha1_pulp_cr.yaml
 
+kubectl apply -f deploy/pulp-operator.config-map.yml
 kubectl apply -f deploy/service_account.yaml
 kubectl apply -f deploy/role.yaml
 kubectl apply -f deploy/role_binding.yaml
