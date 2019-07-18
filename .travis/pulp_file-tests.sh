9704ddbb827dafa58ed6c4f63142ca69f192ae1d
Mike DePaulo <mikedep333@redhat.com>
2019-07-18 14:11:23 -0400

    Test basic Pulp functionality, including writing to disk
    Using pulp_file's sync tests.

---
+++ .travis/pulp_file-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# coding=utf-8
+
+pushd pulp_file/docs/scripts
+# Let's only do sync tests.
+# So as to check that Pulp can work in containers, including writing to disk.
+# If the upload tests are simpler in the long run, just use them.
+source docs_check_sync_publish.sh
+popd
+
