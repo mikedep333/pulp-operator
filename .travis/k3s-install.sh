ab9957df0b1e689845d923adb991eef944475e16
Mike DePaulo <mikedep333@redhat.com>
2019-07-18 11:26:36 -0400

    Add comment on better way to access insecure registries

--- .travis/k3s-install.sh
+++ .travis/k3s-install.sh
@@ -5,7 +5,10 @@
 # Does a bunch of stuff, such as setting up a `kubectl` -> `k3s kubectl` symlink.
 # We want to allow devs to use Pulp's ports, like 80, 24816 or 24817,
 # not just the default 30000-32767.
+#
 # Also, we need to fix access to registry.centos.org
+# TODO: Use this method instead:
+# https://github.com/rancher/k3s/issues/145#issuecomment-490143506
 curl -sfL https://get.k3s.io | sudo INSTALL_K3S_EXEC="--kube-apiserver-arg service-node-port-range=80-32767 --insecure-registry" sh -
 echo "SYSTEMD UNIT:"
 sudo cat /etc/systemd/system/k3s.service
